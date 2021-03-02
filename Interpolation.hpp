b6fe8d5fb6367215ca9ded0da75fa8560091de18
th-snu <54742258+th-snu@users.noreply.github.com>
2021-03-02 18:37:29 +0900

    Added function prototype for interpolation

--- Interpolation.hpp
+++ Interpolation.hpp
@@ -3,9 +3,18 @@
 #include "bvh-loader/BVHReader.h"
 
 // Implementation of interpolation algorithm
+Eigen::Quaterniond bvh_to_quaternion(const double x, const double y, const double z);
 Eigen::Quaterniond bvh_to_quaternion(const Eigen::Vector3d& euler);
 
 Eigen::Vector3d quaternion_to_bvh(const Eigen::Quaterniond& q);
+
+double get_y_rotation(Eigen::Quaterniond curr, Eigen::Quaterniond goal);
+
+Eigen::Quaterniond slerp_frames_orientation(Motion frames);
+
+Eigen::Vector3d resample_pos(const Motion& motion, double frame);
+
+Eigen::Quaterniond resample_ori(const Motion& motion, double frame);
 
 Motion expand_motion(Motion motion, double frames);
 
